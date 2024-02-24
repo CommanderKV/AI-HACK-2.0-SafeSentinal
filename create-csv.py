e6efcc86424f932e036258b3554bec3fad3c3487
CommanderKV <kylervisser@gmail.com>
2024-02-24 05:45:52 -0500

    added files to create a csv

---
+++ create-csv.py
@@ -0,0 +1,81 @@
+import csv
+import time
+import requests
+import asyncio
+
+def fetch_website_content(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.text
+    else:
+        return None
+
+def classify_website(url, type_category):
+    safeCategories = ["benign", "safe"]
+    suspiciousCategories = ['defacement']
+    unsafeCategories = ["malware", "phishing"]
+    if type_category in safeCategories:
+        return 0
+    elif type_category in suspiciousCategories:
+        return 1
+    elif type_category in unsafeCategories:
+        return 2
+    else:
+        print("UNKNOWN THREAT TYPE: ", type_category, "for URL: ", url, "Skipping...")
+        return -1
+
+def write_to_csv(data, filename):
+    with open(filename, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['URL', 'Content', 'Threat'])
+        for url, content, threat in data:
+            writer.writerow([url, content, threat])
+
+def process_csv(filename):
+    website_content = []
+
+    with open(filename, 'r') as file:
+        reader = csv.reader(file)
+        next(reader)  # Skip header row
+        for row in reader:
+            url = row[0]
+
+            # Add missing elements of the URL
+            if "www." not in url[:10]:
+                url = "www." + url
+
+            if ("http" not in url):
+                url = "https://" + url
+            
+            
+            type_category = row[1]
+
+            # Try to get the website content
+            try:
+                content = fetch_website_content(url)
+            except:
+                print("Failed to fetch", url, "with https. Trying http...")
+                try:
+                    url = url.replace("https", "http")
+                    content = asyncio.run(fetch_website_content(url))
+                except:
+                    print("Failed to fetch", url)
+                    content = None
+
+            # Classify the website
+            threat = classify_website(url, type_category)
+            if content and threat != -1:
+                website_content.append((url, content, threat))
+                print("[ADDED]", url, "to the dataset.")
+            
+            time.sleep(.2)
+    
+    return website_content
+
+def main():
+    classified_websites = process_csv('malicious_phish.csv')
+    write_to_csv(classified_websites, 'classified_websites.csv')
+
+if __name__ == '__main__':
+    #print((fetch_website_content('https://www.google.com')))
+    main()
